47f58a29369ae0459ed83ec0b400c339423ca82e
arori <hjmhjmop@gmail.com>
2021-09-06 06:03:09 +0900

    C:/Program Files/Git/admin 管理ページタイトルの修正

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from . import views
+from django.contrib import admin
 
 app_name = 'accounts'
 
@@ -10,3 +11,8 @@ urlpatterns = [
     path('delete_confirm', TemplateView.as_view(template_name='registration/delete_confirm.html'), name='delete-confirmation'),
     path('delete_complete', views.DeleteView.as_view(), name='delete-complete'),
 ]
+
+admin.site.site_header = 'Yes&Home'
+admin.site.site_title = 'Yes&Home'
+admin.site.index_title = 'Yes&Home 管理ページ'
+admin.site.site_urls= '/admin'
